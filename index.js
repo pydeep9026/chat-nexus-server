2760f31234d0c460069a13e694632c8dbbdafd5e
Pradeep Singh <91087103+pydeep9026@users.noreply.github.com>
2023-02-23 21:35:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ const server=app.listen(process.env.PORT,()=>{
 
 const io =socket(server,{      
     cors:{
-        origin:"http://localhost:3000",
+        origin:"https://chatnexustempshut.netlify.app/",
         Credentials:true,
     }  
 })       
